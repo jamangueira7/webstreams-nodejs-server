40616a5824c16b60042de2b89c901ddb3017268d
João Mangueira <jamjs7@gmail.com>
2023-03-06 15:51:27 -0300

    add arquivo e fazendo uma leitura sob demanda

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-console.log("ae")
+import { createServer } from 'node:http'
+import { createReadStream } from 'node:fs'
+import { setTimeout } from 'node:timers/promises'
+import { Transform, Readable } from 'node:stream'
+import { TransformStream, WritableStream } from 'node:stream/web'
+import csvtojson from 'csvtojson'
+
+const PORT = 3000
+createServer(async (request, response) => {
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': '*',
+    }
+
+    if(response.method === 'OPTIONS') {
+        response.writeHead(204, headers)
+        response.end()
+        return
+    }
+
+    let items = 0
+    request.once('close', _ => console.log(`connection was closed!`, items))
+
+    Readable.toWeb(createReadStream('./animeflv.csv'))
+        //o passo a passo que cada item individual vai trafegar
+        .pipeThrough(Transform.toWeb(csvtojson()))
+        .pipeThrough(new TransformStream({
+            transform(chunk, controller) {
+                const data = JSON.parse(Buffer.from(chunk))
+
+                const mappedData = {
+                    title: data.title,
+                    description: data.description,
+                    url_anime: data.url_anime,
+                }
+
+                //quebra de linha pois é um NDJSON
+                controller.enqueue(JSON.stringify(mappedData).concat('\n'))
+            }
+        }))
+        //pipeTo é a ultima etapa
+        .pipeTo(new WritableStream({
+            async write(chunk) {
+                await setTimeout(1000)
+                items ++
+                response.write(chunk)
+            },
+            close() {
+                response.end()
+            }
+        }))
+
+
+    response.writeHead(200, headers)
+})
+.listen(PORT)
+.on('listening', _ => console.log(`server is running at ${PORT}`))
